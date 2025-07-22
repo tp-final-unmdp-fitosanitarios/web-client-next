86d49bfcc84d0f4aecaea6c5b8ca1ee370f331d5
jereSava <savarinojeremias@gmail.com>
2025-07-22 11:45:22 -0300

    Deploy with new styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => {
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
         <p className={styles.footerText}>
-          © {currentYear} PampaGrow. Todos los derechos reservados.
+          © {currentYear}  EPP Sistema de Gestión de Agroquímicos
         </p>
         <div className={styles.footerLinks}>
         </div>
